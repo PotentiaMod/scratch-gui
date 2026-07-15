3b596f92821e295290a8e0797d28dcff48566324
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-07-15 07:52:30 -0400

    u

--- src/containers/blocks.jsx
+++ src/containers/blocks.jsx
@@ -105,7 +105,6 @@ class Blocks extends React.Component {
     constructor (props) {
         super(props);
         this.ScratchBlocks = VMScratchBlocks(props.vm, false);
-		/*
 		this.ScratchBlocks.Toolbox.registerMenu('extensionControls', [
             {
                 text: 'Remove Extension',
@@ -123,7 +122,6 @@ class Blocks extends React.Component {
                 callback: ext => this.props.onOpenCustomExtensionModal(ext)
             }
         ]);
-       */
         window.ScratchBlocks = this.ScratchBlocks;
         AddonHooks.blockly = this.ScratchBlocks;
         AddonHooks.blocklyCallbacks.forEach(i => i());
