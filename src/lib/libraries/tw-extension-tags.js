b93b9138db18b4b5541710eb5932975907971630
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-07-23 04:06:02 -0400

    a

--- src/lib/libraries/tw-extension-tags.js
+++ src/lib/libraries/tw-extension-tags.js
@@ -1,7 +1,7 @@
 import {APP_NAME} from '../brand';
 import messages from './tag-messages.js';
 
-// Because there are all brand names, it is unnecessary for them to be translatable.
+/// Because there are all brand names, it is unnecessary for them to be translatable.
 export default [
     {tag: 'scratch', intlLabel: 'Scratch'},
     {tag: 'potentia', intlLabel: APP_NAME},
