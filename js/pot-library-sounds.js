561579cf3f6df324e7844cd2652227c0b3e58c64
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-08-01 04:39:32 -0400

    [skip ci] Build for $(git log --pretty=format:%H -n1)

---
+++ js/pot-library-sounds.js
@@ -0,0 +1,15 @@
+(window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([["pot-library-sounds"],{
+
+/***/ "./src/lib/libraries/pot-assets/generated-sounds.json":
+/*!************************************************************!*\
+  !*** ./src/lib/libraries/pot-assets/generated-sounds.json ***!
+  \************************************************************/
+/*! exports provided: 0, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("[{\"name\":\"Cat Hiss\",\"tags\":[\"potentia\",\"animals\",\"cats\",\"sounds\"],\"dataFormat\":\"mp3\",\"sampleCount\":101564,\"rate\":48000,\"src\":{\"library\":\"potentia\",\"path\":\"/sounds/Cat%20Hiss.mp3\"}}]");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=pot-library-sounds.js.map
