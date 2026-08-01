79165dcd480e0b836dd523461bcfe80d225f5590
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-08-01 06:42:07 -0400

    UPLOAD CODE

---
+++ build/js/pot-library-backdrops.js
@@ -0,0 +1,15 @@
+(window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([["pot-library-backdrops"],{
+
+/***/ "./src/lib/libraries/pot-assets/generated-backdrops.json":
+/*!***************************************************************!*\
+  !*** ./src/lib/libraries/pot-assets/generated-backdrops.json ***!
+  \***************************************************************/
+/*! exports provided: 0, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("[{\"name\":\"Stripes 2\",\"tags\":[\"potentia\",\"patterns\"],\"bitmapResolution\":1,\"dataFormat\":\"svg\",\"rotationCenterX\":240,\"rotationCenterY\":180,\"src\":{\"library\":\"potentia\",\"path\":\"/backdrops/Stripes%202.svg\"}}]");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=pot-library-backdrops.js.map
---
+++ build/js/pot-library-costumes.js
@@ -0,0 +1,15 @@
+(window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([["pot-library-costumes"],{
+
+/***/ "./src/lib/libraries/pot-assets/generated-costumes.json":
+/*!**************************************************************!*\
+  !*** ./src/lib/libraries/pot-assets/generated-costumes.json ***!
+  \**************************************************************/
+/*! exports provided: 0, 1, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("[{\"name\":\"Raccoon-a\",\"tags\":[\"potentia\",\"animals\",\"raccoons\",\"mascots\"],\"bitmapResolution\":1,\"dataFormat\":\"svg\",\"rotationCenterX\":84.22338,\"rotationCenterY\":89.338776566927,\"src\":{\"library\":\"potentia\",\"path\":\"/costumes/Raccoon-a.svg\"}},{\"name\":\"Raccoon-b\",\"tags\":[\"potentia\",\"animals\",\"raccoons\",\"mascots\"],\"bitmapResolution\":1,\"dataFormat\":\"svg\",\"rotationCenterX\":84.22338,\"rotationCenterY\":89.3387857834635,\"src\":{\"library\":\"potentia\",\"path\":\"/costumes/Raccoon-b.svg\"}}]");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=pot-library-costumes.js.map
