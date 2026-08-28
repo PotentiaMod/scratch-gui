767d04c43e90364c7271a7246b4b22a5f297ab38
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-08-28 01:29:52 -0400

    UPLOAD CODE

---
+++ src/lib/udrobot-modify/udpi.js
@@ -0,0 +1,153 @@
+export default function (Blockly) {
+    
+    // Blockly.Blocks['udblockUDPi_whenButtonPressed'] = {
+    //     /**
+    //      * Block for repeat n times (external number).
+    //      * https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#5eke39
+    //      * @this Blockly.Block
+    //      */
+    //     init: function () {
+    //         this.jsonInit({
+    //             "id": "udblockUDPi_whenButtonBPressed",
+    //             "message0": "当主板按钮%1按下时",
+    //             "message1": "执行 %1", // Statement
+    //             "lastDummyAlign2": "RIGHT",
+    //             "args0": [
+    //                 {
+    //                     "type": "field_dropdown",
+    //                     "name": "btn",
+    //                     "options": [
+    //                         ["A", "0"],
+    //                         ["B", "2"]
+    //                     ]
+    //                 }
+    //             ],
+    //             "args1": [
+    //                 {
+    //                     "type": "input_statement",
+    //                     "name": "DO"
+    //                 }
+    //             ],
+    //             "category": Blockly.Categories.udblockUDPi,
+    //             "extensions": ["colours_control", "shape_hat"]
+    //         });
+    //     }
+    // };
+
+    // 控制主板蜂鸣器播放
+    // Blockly.Blocks['udblockUDPi_setBuzzerPlay'] = {
+    //     init: function () {
+    //         this.jsonInit({
+    //             "id": "udblockUDPi_setBuzzerPlay",
+    //             "message0": "蜂鸣器播放声音%1 音调%2",
+    //             "args0": [
+    //                 {
+    //                     "type": "field_dropdown",
+    //                     "name": "sound",
+    //                     "options": [
+    //                         [
+    //                             "DO",
+    //                             "DO"
+    //                         ],
+    //                         [
+    //                             "RE",
+    //                             "RE"
+    //                         ],
+    //                         [
+    //                             "MI",
+    //                             "MI"
+    //                         ],
+    //                         [
+    //                             "FA",
+    //                             "FA"
+    //                         ],
+    //                         [
+    //                             "SO",
+    //                             "SO"
+    //                         ],
+    //                         [
+    //                             "LA",
+    //                             "LA"
+    //                         ],
+    //                         [
+    //                             "SI",
+    //                             "SI"
+    //                         ]
+    //                     ]
+    //                 },
+    //                 {
+    //                     "type": "field_dropdown",
+    //                     "name": "mode",
+    //                     "options": [
+    //                         [
+    //                             "低音",
+    //                             "LOW"
+    //                         ],
+    //                         [
+    //                             "中音",
+    //                             "MID"
+    //                         ],
+    //                         [
+    //                             "高音",
+    //                             "HIGH"
+    //                         ]
+    //                     ]
+    //                 }
+    //             ],
+    //             "category": Blockly.Categories.udblockUDPi,
+    //             "inputsInline": true,
+    //             "extensions": ["colours_control", "shape_statement", "scratch_extension"]
+    //         })
+    //     }
+    // }
+
+    // Blockly.Blocks['udblockUDPi_setRGBDraw'] = {
+    //     init: function(){
+    //         this.jsonInit({
+    //             "type": "rgb_board_clear_all",
+    //             "message0": "刷新所有RGB为%1",
+    //             "args0": [
+    //                 {
+    //                     "type": "input_value",
+    //                     "name": "COLOR",
+    //                 }
+    //             ],
+    //             "category": Blockly.Categories.udblockUDPi,
+    //             "inputsInline": true,
+    //             "extensions": ["colours_control", "shape_statement", "scratch_extension"]
+    //         })
+    //     }
+    // }
+
+
+    // Blockly.Blocks['udblockUDPi_closeConnectToWiFi'] = {
+    //     /**
+    //      * Block for repeat n times (external number).
+    //      * https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#5eke39
+    //      * @this Blockly.Block
+    //      */
+    //     init: function () {
+    //         this.jsonInit({
+    //             "id": "udblockUDPi_closeConnectToWiFi",
+    //             "message1": "断开WIFI连接",
+    //             "message0": "  %1 ",
+    //             "lastDummyAlign2": "LEFT",
+    //             "args0": [
+    //                 {
+    //                     "type": "field_image",
+    //                     "src": Blockly.mainWorkspace.options.pathToMedia + "repeat.svg",
+    //                     "width": 24,
+    //                     "height": 24,
+    //                     "alt": "*",
+    //                     "flip_rtl": true
+    //                 }
+    //             ],
+    //             "category": Blockly.Categories.udblockUDPi,
+    //             "inputsInline": true,
+    //             "extensions": ["colours_control", "shape_statement", "scratch_extension"]
+
+    //         });
+    //     }
+    // };
+
+}
