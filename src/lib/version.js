7ff04dd401dde1ea2f682ffbd4b5615c377d5ba6
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-09-08 23:19:07 -0400

    Agu!

--- src/lib/version.js
+++ src/lib/version.js
@@ -1,6 +1,6 @@
 // Legacy export format because this is used by some build-time scripts stuck in the past.
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-    APP_VERSION: '1.20.0',
-    DESKTOP_VERSION: '1.19.7'
+    APP_VERSION: '1.2.0',
+    DESKTOP_VERSION: '1.1.9'
 };
