dff058f38cb3e32f2030a756aaf8d266464a2287
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-09-25 16:53:22 -0400

    a

--- upload.sh
+++ upload.sh
@@ -4,3 +4,11 @@ git commit -m "UPLOAD CODE"
 git branch -M develop
 git remote add origin https://github.com/PotentiaMod/scratch-gui.git
 git push -f origin develop
+
+cd build
+git init
+git add .
+git commit -m "UPLOAD CODE"
+git branch -M offline-build
+git remote add origin https://github.com/PotentiaMod/scratch-gui.git
+git push -f origin offline-build
